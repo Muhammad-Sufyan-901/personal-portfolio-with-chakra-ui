7b1b5065d0b24fdc1b9c32dc7b58fd8a011f98e7
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2023-07-22 12:24:35 +0800

    Adding Lazy Load For Whole Section On MainPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
+import dynamic, { LoaderComponent } from "next/dynamic";
 import { Box } from "@chakra-ui/react";
-import {
-  HomeSectionComponent,
-  AboutSectionComponent,
-  SkillsSectionComponent,
-  EducationSectionComponent,
-  ExperienceSectionComponent,
-  PortfolioSectionComponent,
-  GetInTouchComponent,
-  BlogSectionComponent,
-  ContactSectionComponent,
-} from "@/components";
+
+const HomeSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/HomeSectionComponent"));
+const AboutSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/AboutSectionComponent"));
+const SkillsSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/SkillsSectionComponent"));
+const EducationSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/EducationSectionComponent"));
+const ExperienceSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/ExperienceSectionComponent"));
+const PortfolioSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/PortfolioSectionComponent"));
+const GetInTouchComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/GetInTouchComponent"));
+const BlogSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/BlogSectionComponent"));
+const ContactSectionComponent: React.ComponentType = dynamic((): LoaderComponent => import("@/components/ContactSectionComponent"));
 
 export default function MainPage(): React.JSX.Element {
   return (
