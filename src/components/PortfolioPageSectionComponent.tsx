577de542117990c429cea85584b0951367c85af2
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2023-07-24 11:58:34 +0800

    Adding Transition Animation On PortfolioPageSectionComponent And PortfolioSectionComponent

--- src/components/PortfolioPageSectionComponent.tsx
+++ src/components/PortfolioPageSectionComponent.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import { motion } from "framer-motion";
 import { Box, Center, Container, Flex, Grid, GridItem } from "@chakra-ui/react";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 import { PortfolioCard, PrimaryButton, SectionDescription, SectionSubtitle, SectionTitle } from "@/fragments";
 import { portfolioList } from "@/constants";
+import { fadeInTransition, staggeredContainer } from "@/utils";
 import SectionLayout from "@/layouts/SectionLayout";
 
 function PortfolioPageSectionComponent(): React.JSX.Element {
@@ -17,9 +19,18 @@ function PortfolioPageSectionComponent(): React.JSX.Element {
       <SectionTitle title="Portfolio" />
 
       {/* Portfolio Page Section Content */}
-      <Container maxWidth={{ base: "container.lg", lg: "75rem" }}>
+      <Container
+        as={motion.div}
+        variants={staggeredContainer(0.1, 0.1)}
+        viewport={{ once: true }}
+        initial="hidden"
+        whileInView="show"
+        maxWidth={{ base: "container.lg", lg: "75rem" }}
+      >
         {/* Portfolio Page Section Subtitle & Description */}
         <Flex
+          as={motion.div}
+          variants={fadeInTransition("up", "tween", 0.1, 1.1)}
           direction="column"
           rowGap={{ base: 3, lg: 5 }}
         >
@@ -29,6 +40,8 @@ function PortfolioPageSectionComponent(): React.JSX.Element {
 
         {/* Portfolio Page Section Content Container */}
         <Grid
+          as={motion.div}
+          variants={fadeInTransition("left", "tween", 0, 1)}
           templateColumns={{ base: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
           gap={8}
           width={{ base: "90%", lg: "100%" }}
@@ -37,7 +50,11 @@ function PortfolioPageSectionComponent(): React.JSX.Element {
         >
           {portfolioList.map(
             ({ title, imageThumbnail, description, livePreviewURL, repositoryURL, techStack }, index): React.ReactNode => (
-              <GridItem key={`${title} - ${index}`}>
+              <GridItem
+                as={motion.div}
+                variants={fadeInTransition("up", "tween", index * 0.25, 1.1)}
+                key={`${title} - ${index}`}
+              >
                 <PortfolioCard
                   title={title}
                   imageThumbnail={imageThumbnail}
@@ -52,7 +69,11 @@ function PortfolioPageSectionComponent(): React.JSX.Element {
         </Grid>
 
         {/* Portfolio Page Section Redirect Button */}
-        <Center marginTop={20}>
+        <Center
+          as={motion.div}
+          variants={fadeInTransition("up", "tween", 0.5, 1.1)}
+          marginTop={20}
+        >
           <PrimaryButton
             href="/#portfolio"
             variant="solid"
--- src/components/PortfolioSectionComponent.tsx
+++ src/components/PortfolioSectionComponent.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { motion } from "framer-motion";
 import { Box, Center, Container, Flex, Grid, GridItem } from "@chakra-ui/react";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 import { PortfolioCard, PrimaryButton, SectionDescription, SectionSubtitle, SectionTitle } from "@/fragments";
 import { portfolioList } from "@/constants";
-import { truncateList } from "@/utils";
+import { fadeInTransition, staggeredContainer, truncateList } from "@/utils";
 import SectionLayout from "@/layouts/SectionLayout";
 import type { Portfolio } from "@/types";
 
@@ -22,9 +23,18 @@ function PortfolioSectionComponent(): React.JSX.Element {
       <SectionTitle title="Portfolio" />
 
       {/* Portfolio Section Content */}
-      <Container maxWidth={{ base: "container.lg", lg: "75rem" }}>
+      <Container
+        as={motion.div}
+        variants={staggeredContainer(0.1, 0.1)}
+        viewport={{ once: true, amount: 0.25 }}
+        initial="hidden"
+        whileInView="show"
+        maxWidth={{ base: "container.lg", lg: "75rem" }}
+      >
         {/* Portfolio Section Subtitle & Description */}
         <Flex
+          as={motion.div}
+          variants={fadeInTransition("up", "tween", 0.1, 1.1)}
           direction="column"
           rowGap={{ base: 3, lg: 5 }}
         >
@@ -34,6 +44,8 @@ function PortfolioSectionComponent(): React.JSX.Element {
 
         {/* Portfolio Section Content Container */}
         <Grid
+          as={motion.div}
+          variants={fadeInTransition("left", "tween", 0, 1)}
           templateColumns={{ base: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
           gap={8}
           width={{ base: "90%", lg: "100%" }}
@@ -42,7 +54,11 @@ function PortfolioSectionComponent(): React.JSX.Element {
         >
           {truncatedPortfolioList.map(
             ({ title, imageThumbnail, description, livePreviewURL, repositoryURL, techStack }, index): React.ReactNode => (
-              <GridItem key={`${title} - ${index}`}>
+              <GridItem
+                as={motion.div}
+                variants={fadeInTransition("up", "tween", index * 0.25, 1.1)}
+                key={`${title} - ${index}`}
+              >
                 <PortfolioCard
                   title={title}
                   imageThumbnail={imageThumbnail}
@@ -56,7 +72,11 @@ function PortfolioSectionComponent(): React.JSX.Element {
           )}
         </Grid>
 
-        <Center marginTop={10}>
+        <Center
+          as={motion.div}
+          variants={fadeInTransition("up", "tween", 0.5, 1.1)}
+          marginTop={10}
+        >
           <PrimaryButton
             href="/portfolio"
             variant="solid"
