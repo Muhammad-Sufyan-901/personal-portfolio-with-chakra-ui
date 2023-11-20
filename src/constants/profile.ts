8eeaec048b703b92892e7cbb692bf3b93832a572
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2023-11-20 08:36:06 +0800

    Updating Profile Data

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -8,19 +8,19 @@ export const profileData: Profile = {
   pictures: ["/assets/images/home-profile.webp", "/assets/images/about-profile.webp"],
   descriptions: [
     "I create stunning website for your business, Intermediate experienced in web design and development.",
-    "Hello everyone! I am Muhammad Sufyan, a frontend mobile and website developer. I am a intermediate experienced and I hope with this I can always learning something new to be able to invent digital applications that can help many people in the future. Can work alone or in a team.",
+    "Hello everyone! I am Muhammad Sufyan, a frontend mobile and website developer. I am a intermediate experienced and I hope with this I can always learning something new to be able to invent digital applications that can help many people in the future. Could work independently or in a team.",
   ],
   experiences: [
     {
-      number: 2,
+      number: 3,
       title: "Years Of Experience",
     },
     {
       number: 4,
-      title: "Programming Language Used",
+      title: "Framework & Tech Stack Used",
     },
     {
-      number: 6,
+      number: 8,
       title: "Successed Projects",
     },
   ],
