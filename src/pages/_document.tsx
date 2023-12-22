76688d3096cbf4c014af032eda888566388922c9
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2023-12-22 20:08:58 +0800

    Adding suppressHydrationWarning Attribute To Avoid Hydration Error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document(): React.JSX.Element {
   return (
-    <Html lang="en">
+    <Html
+      lang="en"
+      suppressHydrationWarning
+    >
       <Head />
       <body>
         <Main />
