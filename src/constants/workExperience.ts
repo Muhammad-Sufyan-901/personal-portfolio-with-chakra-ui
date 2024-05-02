63022c232212f1c195cdd8c82becb3de8eb1a71b
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2024-05-03 02:15:09 +0800

    Update dates on work experiences

--- src/constants/workExperience.ts
+++ src/constants/workExperience.ts
@@ -4,7 +4,7 @@ export const workExperienceList: WorkExperience[] = [
   {
     jobDesk: "Mobile Developer",
     companyName: "Global Digital Verse",
-    years: "2024 - Present",
+    years: "February 2024 - Present",
     jobStatus: "Full Time",
     jobDescription: "Responsible for creating whole mobile apps such as Tampang.com, Digital Salesman and many more. Responsible for creating whole user interface and integrate the API.",
     techStack: ["/assets/svg/flutter.svg", "/assets/svg/dart.svg", "/assets/svg/firebase.svg", "/assets/svg/react native.svg", "/assets/svg/typescript.svg"],
@@ -12,7 +12,7 @@ export const workExperienceList: WorkExperience[] = [
   {
     jobDesk: "Full Stack Web Developer",
     companyName: "Global Digital Verse",
-    years: "2023 - Present",
+    years: "May 2023 - Present",
     jobStatus: "Full Time",
     jobDescription: "Responsible for creating several features found on websites such as FindDW, Optimus, Litani and many more and responsible for creating the entire company profile website.",
     techStack: ["/assets/svg/laravel.svg", "/assets/svg/bootstrap.svg", "/assets/svg/mysql.svg", "/assets/svg/php.svg"],
@@ -20,7 +20,7 @@ export const workExperienceList: WorkExperience[] = [
   {
     jobDesk: "Frontend Developer",
     companyName: "ZettaByte Pte Ltd",
-    years: "2021 - 2022",
+    years: "January 2022 - April 2022",
     jobStatus: "Internship",
     jobDescription: "Responsible for making several features found on the ADMTC and EDH platforms such as Search Filters and Form Validation.",
     techStack: ["/assets/svg/angular.svg", "/assets/svg/typescript.svg", "/assets/svg/graphql.svg"],
@@ -28,7 +28,7 @@ export const workExperienceList: WorkExperience[] = [
   {
     jobDesk: "Quality Assurance",
     companyName: "ZettaByte Pte Ltd",
-    years: "2021 - 2022",
+    years: "January 2022 - April 2022",
     jobStatus: "Internship",
     jobDescription: "Responsible for debugging some features and testing on the ADMTC and EDH platforms",
     techStack: ["/assets/svg/jira.svg", "/assets/svg/docs.svg"],
